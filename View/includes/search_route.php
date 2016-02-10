a2fdfc7402a27f9e1b21acb419742deec97e9f40
Justin Borg <justinborg1994@gmail.com>
2016-02-10 11:22:49 +0100

    Edit Search Route View

--- View/includes/search_route.php
+++ View/includes/search_route.php
@@ -1,57 +1,66 @@
 <div class="row" style="margin-left: 70px; margin-right: 70px; margin-top: 70px">
-    <div class="col-sm-4" style="padding: 15px; background-color: #F2F2F2;">
-        <h4><b>New Search:<b></h4>
-        <br>
-        <form class="form-horizontal">
-            <div class="form-group">
-                <label for="dest_from" class="col-sm-2 control-label">From:</label>
-                <div class="col-sm-10">
-                    <div class="input-group">
-							<span class="input-group-addon">
-								<i class="fa fa-circle-o" style="color:green"></i>
-							</span>
-                        <input type="text" class="form-control" id="from">
+    <div class="col-sm-4" style="padding: 15px;">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><b>Trip Details</b></h3>
+            </div>
+            <div class="panel-body">
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="dest_from" class="col-sm-2 control-label">From:</label>
+                        <div class="col-sm-10">
+                            <div class="input-group">
+									<span class="input-group-addon">
+										<i class="fa fa-circle-o" style="color:green"></i>
+									</span>
+                                <input type="text" class="form-control" id="from">
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="dest_to" class="col-sm-2 control-label">To:</label>
-                <div class="col-sm-10">
-                    <div class="input-group">
-							<span class="input-group-addon">
-								<i class="fa fa-circle-o" style="color:red"></i>
-							</span>
-                        <input type="text" class="form-control" id="to">
+                    <div class="form-group">
+                        <label for="dest_to" class="col-sm-2 control-label">To:</label>
+                        <div class="col-sm-10">
+                            <div class="input-group">
+									<span class="input-group-addon">
+										<i class="fa fa-circle-o" style="color:red"></i>
+									</span>
+                                <input type="text" class="form-control" id="to">
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="date" class="col-sm-2 control-label">Date:</label>
-                <div class="col-sm-10">
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="date">
-							<span class="input-group-addon">
-								<i class="glyphicon glyphicon-calendar"></i>
-							</span>
+                    <div class="form-group">
+                        <label for="date" class="col-sm-2 control-label">Date:</label>
+                        <div class="col-sm-10">
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="date">
+									<span class="input-group-addon">
+										<i class="glyphicon glyphicon-calendar"></i>
+									</span>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
-            <hr style="color: dimgray; background-color: dimgray; height: 2px;">
-            <h4><b>Driver Details:<b></h4>
-            <br>
-            <div class="form-group">
-                <label for="driver_name" class="col-sm-2 control-label">Name:</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="driver_name">
-                </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><b>Driver Details</b></h3>
             </div>
-            <div class="form-group">
-                <label for="driver_rating" class="col-sm-2 control-label">Rating:</label>
-                <input id="input-2c" class="rating" min="0" max="5" step="0.5" data-size="xs" data-symbol="&#xf005;" data-glyphicon="false" data-rating-class="rating-fa" data-show-clear="false">
+            <div class="panel-body">
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="driver_name" class="col-sm-2 control-label">Name:</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="driver_name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="driver_rating" class="col-sm-2 control-label">Rating:</label>
+                        <input id="input-2c" class="rating" min="0" max="5" step="0.5" data-size="xs" data-symbol="&#xf005;" data-glyphicon="false" data-rating-class="rating-fa" data-show-clear="false">
+                    </div>
+                </form>
             </div>
-            <hr style="color: dimgray; background-color: dimgray; height: 2px;">
-            <button type="submit" class="btn btn-primary" style="float: right"><b>Search</b></button>
-        </form>
+        </div>
     </div>
     <div class="col-sm-7 col-sm-offset-1 text-center" style="padding: 15px; background-color: #F2F2F2;">
         <h2>This is were matching routes will be shown</h2>
